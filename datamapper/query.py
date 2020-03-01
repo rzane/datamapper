44f310590742dd9e834a9f35eafd203a737ccb30
Ray Zane <raymondzane@gmail.com>
2020-03-01 17:31:00 -0500

    Build more complicated queries

--- datamapper/query.py
+++ datamapper/query.py
@@ -1,42 +1,108 @@
+from typing import Any, List, Union
 from datamapper.model import Model
-from sqlalchemy import desc
-from sqlalchemy.sql.expression import Select
+from sqlalchemy import and_, or_, Column
+from sqlalchemy.sql.expression import Select, TextClause
 
 
 class Query:
     model: Model
-    _value: Select
+    _where: Any
+    _limit: Union[int, None]
+    _offset: Union[int, None]
+    _order_by: List[str]
 
-    def __init__(self, model: Model, _statement: Select = None):
+    def __init__(self, model: Model):
         self.model = model
+        self._limit = None
+        self._offset = None
+        self._where = None
+        self._order_by = []
 
-        if _statement is not None:
-            self.statement = _statement
-        else:
-            self.statement = self.model.__table__.select()
+    def build(self):
+        statement = self.model.__table__.select()
+
+        if self._limit is not None:
+            statement = statement.limit(self._limit)
+
+        if self._offset is not None:
+            statement = statement.offset(self._offset)
+
+        if self._where is not None:
+            statement = statement.where(self._where)
+
+        if self._order_by:
+            statement = statement.order_by(*self._order_by)
+
+        return statement
 
     def limit(self, value):
-        return Query(self.model, self.statement.limit(value))
+        query = self._clone()
+        query._limit = value
+        return query
 
     def offset(self, value):
-        return Query(self.model, self.statement.offset(value))
+        query = self._clone()
+        query._offset = value
+        return query
 
-    def where(self, **conditions):
-        statement = self.statement
-        columns = self.model.__table__.columns
+    def where(self, *args, **kwargs):
+        query = self._clone()
+        exprs = self._build_where(*args, **kwargs)
+        if query._where is None:
+            query._where = and_(*exprs)
+        else:
+            query._where = and_(query._where, *exprs)
+        return query
 
-        for key, value in conditions.items():
-            column = getattr(columns, key)
+    def or_where(self, *args, **kwargs):
+        query = self._clone()
+        exprs = self._build_where(*args, **kwargs)
+        if query._where is None:
+            query._where = and_(*exprs)
+        else:
+            query._where = or_(query._where, and_(*exprs))
+        return query
 
+    def order_by(self, *args):
+        exprs = []
+
+        for arg in args:
+            if isinstance(arg, TextClause):
+                exprs.append(arg)
+            elif isinstance(arg, str) and arg.startswith("-"):
+                column = self._get_column(arg[1:])
+                exprs.append(column.desc())
+            elif isinstance(arg, str):
+                column = self._get_column(arg)
+                exprs.append(column.asc())
+
+        query = self._clone()
+        query._order_by = query._order_by + exprs
+        return query
+
+    def _build_where(self, *args, **kwargs):
+        exprs = []
+
+        for arg in args:
+            if isinstance(arg, TextClause):
+                exprs.append(arg)
+
+        for name, value in kwargs.items():
+            column = self._get_column(name)
             if isinstance(value, list):
-                statement = statement.where(column.in_(value))
+                exprs.append(column.in_(value))
             else:
-                statement = statement.where(column == value)
+                exprs.append(column == value)
 
-        return Query(self.model, statement)
+        return exprs
 
-    def order_by(self, value):
-        if value.startswith("-"):
-            value = desc(value[1:])
+    def _get_column(self, name) -> Column:
+        return getattr(self.model.__table__.columns, name)
 
-        return Query(self.model, self.statement.order_by(value))
+    def _clone(self):
+        query = Query(self.model)
+        query._where = self._where
+        query._limit = self._limit
+        query._offset = self._offset
+        query._order_by = self._order_by
+        return query
--- tests/test_query.py
+++ tests/test_query.py
@@ -1,3 +1,4 @@
+from sqlalchemy import text
 from sqlalchemy.sql.expression import Select
 from datamapper import Query
 from tests.models import User, to_sql
@@ -6,29 +7,65 @@ from tests.models import User, to_sql
 def test_query():
     query = Query(User)
     assert query.model == User
-    assert isinstance(query.statement, Select)
+
+
+def test_build():
+    query = Query(User).build()
+    assert isinstance(query, Select)
+    assert "SELECT" in to_sql(query)
+    assert "FROM users" in to_sql(query)
 
 
 def test_limit():
     query = Query(User).limit(29)
-    assert "LIMIT 29" in to_sql(query.statement)
+    assert "LIMIT 29" in to_sql(query.build())
 
 
 def test_offset():
     query = Query(User).offset(29)
-    assert "OFFSET 29" in to_sql(query.statement)
+    assert "OFFSET 29" in to_sql(query.build())
 
 
 def test_where():
-    query = Query(User).where(id=9, name="Ray")
-    assert "WHERE users.id = 9 AND users.name = 'Ray'" in to_sql(query.statement)
+    query = Query(User).where(id=1)
+    assert "WHERE users.id = 1" in to_sql(query.build())
+
+
+def test_where_list():
+    query = Query(User).where(id=[1, 2])
+    assert "WHERE users.id IN (1, 2)" in to_sql(query.build())
+
+
+def test_multi_where():
+    query = Query(User).where(id=1, name="Ray")
+    assert "WHERE users.id = 1 AND users.name = 'Ray'" in to_sql(query.build())
+
+
+def test_and_where():
+    query = Query(User).where(id=1).where(name="Ray")
+    assert "WHERE users.id = 1 AND users.name = 'Ray'" in to_sql(query.build())
+
+
+def test_or_where():
+    query = Query(User).where(id=1).or_where(id=2)
+    assert "WHERE users.id = 1 OR users.id = 2" in to_sql(query.build())
+
+
+def test_where_literal():
+    query = Query(User).where(text("users.id = 7"))
+    assert "WHERE users.id = 7" in to_sql(query.build())
 
 
 def test_order_by():
     query = Query(User).order_by("name")
-    assert "ORDER BY users.name" in to_sql(query.statement)
+    assert "ORDER BY users.name ASC" in to_sql(query.build())
 
 
 def test_order_by_desc():
     query = Query(User).order_by("-name")
-    assert "ORDER BY users.name DESC" in to_sql(query.statement)
+    assert "ORDER BY users.name DESC" in to_sql(query.build())
+
+
+def test_order_by_literal():
+    query = Query(User).order_by(text("1"))
+    assert "ORDER BY 1" in to_sql(query.build())
