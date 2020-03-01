d364136bd423c4cfe73935f3cf58c432a74eb731
Ray Zane <raymondzane@gmail.com>
2020-03-01 13:51:40 -0500

    Start setting up tests

--- README.md
+++ README.md
@@ -9,12 +9,3 @@ To install dependencies and setup your development environment, run:
 To run the test suite, run:
 
     $ bin/test
-
-To start the server, run:
-
-    $ bin/start
-
-To start an interactive console session, run:
-
-    $ bin/console
-
---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import os
+import pytest
+from databases import Database
+from sqlalchemy import MetaData
+from sqlalchemy_utils import create_database, database_exists
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://localhost/datamapper")
+
+metadata = MetaData()
+database = Database(DATABASE_URL, force_rollback=True)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_database():
+    engine = create_engine(DATABASE_URL)
+
+    if not database_exists(engine.url):
+        create_database(engine.url)
+
+    metadata.create_all(engine)
