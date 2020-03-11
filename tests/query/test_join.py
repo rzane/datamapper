f288e2a0511943bd77da1b98c3f732a22ac29a27
Ray Zane <raymondzane@gmail.com>
2020-03-11 12:29:58 -0400

    Build up a tree of hashable join nodes

---
+++ tests/query/test_join.py
@@ -0,0 +1,81 @@
+import pytest
+from datamapper.errors import MissingJoinError
+from datamapper.query.join import Join, to_join_tree
+from tests.support import User, Pet
+
+
+def test_join() -> None:
+    join = Join(User, ["pets"])
+    assert join.base == User
+    assert join.path == ["pets"]
+
+
+def test_join_name() -> None:
+    join = Join(User, ["pets"])
+    assert join.name == "pets"
+
+
+def test_join_name_nested() -> None:
+    join = Join(User, ["pets", "owner"])
+    assert join.name == "pets.owner"
+
+
+def test_join_association() -> None:
+    join = Join(User, ["pets"])
+    assert join.association == User.association("pets")
+
+
+def test_join_association_nested() -> None:
+    join = Join(User, ["pets", "owner"])
+    assert join.association == Pet.association("owner")
+
+
+def test_join_equality() -> None:
+    join1 = Join(User, ["pets"])
+    join2 = Join(User, ["pets"])
+
+    assert join1 == join2
+    assert join2 == join1
+
+
+def test_join_inequality() -> None:
+    pets = Join(User, ["pets"])
+    home = Join(User, ["home"])
+
+    assert pets != home
+    assert home != pets
+
+
+def test_join_inquality_other() -> None:
+    join = Join(User, ["pets"])
+    assert join != 100
+
+
+def test_join_hash() -> None:
+    pets = Join(User, ["pets"])
+    owner = Join(User, ["pets", "owner"])
+    joins: dict = {pets: {owner: {}}}
+
+    assert pets in joins
+    assert owner in joins[pets]
+    assert joins[pets][owner] == {}
+
+
+def test_join_repr() -> None:
+    join = Join(User, ["pets", "owner"])
+    assert repr(join) == '<Join base=User name="pets.owner">'
+
+
+def test_to_join_tree() -> None:
+    pets = Join(User, ["pets"])
+    owner = Join(User, ["pets", "owner"])
+    tree = to_join_tree([pets, owner])
+    assert tree[pets][owner] == {}
+
+
+def test_to_join_tree_missing() -> None:
+    owner = Join(User, ["pets", "owner"])
+    message = "Can't join 'pets.owner' without joining 'pets'"
+
+    with pytest.raises(MissingJoinError, match=message):
+        to_join_tree([owner])
