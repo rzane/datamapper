f59113c74f1f136605ba764746e7ea050ed8f8e4
Ray Zane <raymondzane@gmail.com>
2020-03-12 00:48:23 -0400

    Setup sphinx

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+master_doc = "index"
+project = "datamapper"
+copyright = "2020, Ray Zane"
+author = "Ray Zane"
+release = "0.1.0"
+version = "0.1.0"
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+]
